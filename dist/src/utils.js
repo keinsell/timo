8903a791d40cc533567825c37539e0521e64fc95
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-23 16:24:24 +0100

    chore: remove `dist` directory

--- dist/src/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGODB_URL = exports.HOST = exports.PORT = void 0;
-exports.PORT = process.env.PORT || 1337;
-exports.HOST = process.env.HOST || 'localhost';
-exports.MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost/timo';
