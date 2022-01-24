4427e1950e04525a1ba6620da5addbb565f63a0a
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-24 19:02:52 +0100

    test(`timeblocks`): boilerplate future tests

---
+++ __tests__/timeblocks.ts
@@ -0,0 +1,15 @@
+import ava from 'ava'
+
+ava.todo('GET /t/username should return active timeblock')
+ava.todo('GET /t/username should return archive with timeblocks')
+ava.todo('POST /t/username should create new timeblock')
+ava.todo('POST /t/username should not create new timeblock')
+ava.todo('PATCH /t/username should update actual timeblock')
+ava.todo('DELETE /t/username should discard actual timeblock')
+
+ava.todo('GET /t/timeblock should return info about timeblock')
+ava.todo('PATCH /t/timeblock should update timeblock')
+ava.todo('DELETE /t/timeblock should delete timeblock')
+
+ava.todo('POST /t/username/s should return summary from selected range')
+ava.todo('POST /t/username/s should return summary from past week')
