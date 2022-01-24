128e0c78ebb57d37133359fac598748a1e63eb40
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-24 12:30:35 +0100

    feat(`post-timeblock`): add handler for starting timeblock

--- src/timetracking/service.ts
+++ src/timetracking/service.ts
@@ -2,6 +2,8 @@ import express, { Request, Response, Router } from 'express'
 import { User } from 'users/model'
 import { Timeblock } from 'timetracking/model'
 
+import ms from 'ms'
+
 class TimetrackingController {
 	/** Returns information about current Timeblock. */
 	async GET(req: Request, res: Response) {
@@ -14,11 +16,46 @@ class TimetrackingController {
 		// Get current timeblock related to user
 		const timeblock = await Timeblock.findOne({ isTracking: true })
 		if (!timeblock) return res.status(404).json({ status: 'No active timeblock found' })
+
+		const createdAt = timeblock.createdAt.getTime()
+		const actualDate = Date.now()
+		const diff = actualDate - createdAt
+		const duration = ms(diff)
+
+		// await Timeblock.findByIdAndUpdate(timeblock._id, { ...duration })
+		timeblock.duration = duration
+		await timeblock.save()
+
 		return res.status(200).json(timeblock)
 	}
 
 	/** Creates a new timeblock starting from actual date. */
-	async POST() {}
+	async POST(req: Request, res: Response) {
+		const { username } = req.params
+
+		// Check if user exists in database
+		const users = await User.findOne({ username: username }).count()
+		if (!users) return res.status(404).json({ status: 'User not found' })
+
+		// If Return error when timeblock is already running
+		const runningTimeblock = await Timeblock.findOne({ isTracking: true })
+		if (runningTimeblock) return res.status(300).json({ status: 'Timeblock is actually running' })
+
+		// Create new timeblock related to user (from actual Date)
+		const timeblock = await Timeblock.create({
+			user: username,
+			createdAt: new Date(),
+			isTracking: true,
+		})
+
+		if (req.body.description) {
+			timeblock.description = req.body.description
+			await timeblock.save()
+		}
+
+		res.status(201).json(timeblock)
+	}
+
 	/** Updates timeblock, this action generally stands for stopping timeblock or editing description of past timeblock. */
 	async PUT() {}
 	/** Discards timeblock */
@@ -38,5 +75,6 @@ export class TimetracingService {
 
 	private routes() {
 		this.router.get('/:username', this.controller.GET)
+		this.router.post('/:username', this.controller.POST)
 	}
 }
